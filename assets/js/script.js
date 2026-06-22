0ce71335d3172c725e00c9eb3df207b9c30426ff
Josefa Leyton <jleytonmendez@gmail.com>
2026-06-21 21:24:04 -0400

    add alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 $(function() {
     $('#tooltipjs').tooltip();
 });
+
+$(function() {
+    $('#tooltipjs').click(function() {
+        alert("¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.")
+    })
+});
