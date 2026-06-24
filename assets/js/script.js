e274fa13effa43e7ba8f1579da6758bdf8237aa2
Josefa Leyton <jleytonmendez@gmail.com>
2026-06-23 20:29:24 -0400

    documentar script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+// Inicializa el tooltip en el botón del formulario. 
 $(function() {
     $('#tooltipjs').tooltip();
 });
 
+//Muestra una alerta al hacer click en el botón con id=tooltipjs (que dice enviar en el formulario de contacto). 
 $(function() {
     $('#tooltipjs').click(function() {
         alert("¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.")
